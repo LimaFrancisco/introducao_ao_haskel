81a85e3fdd61170b288286f1ef6f6424b76178e8
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-04-14 22:36:50 -0300

    corrigindo erro de sintaxe

--- 01_primeiro.hs
+++ 01_primeiro.hs
@@ -2,5 +2,5 @@
 -- Objetivo: Calcular o quadrado de um numero
 -- Arquivo: Primeiro.hs
 -----------------------------------------------
-quadrado :: Int > Int
+quadrado :: Int -> Int
 quadrado numero = numero * numero
